b475f4ff238df7547c7d06d355729e6eb7ecc857
Michael Luo <405726+wl21st@users.noreply.github.com>
2022-02-28 21:09:48 -0800

    Remove the slow API to get the hostname

--- backside/src/main/java/main/BackSideApplication.java
+++ backside/src/main/java/main/BackSideApplication.java
@@ -1,8 +1,5 @@
 package main;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,47 +9,64 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 @SpringBootApplication
 public class BackSideApplication {
 
-	private static Logger log = LoggerFactory.getLogger(BackSideApplication.class);
+    private static Logger log = LoggerFactory.getLogger(BackSideApplication.class);
 
-	@Autowired
-	Environment environment;
+    @Autowired
+    private Environment environment;
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackSideApplication.class, args);
-	}
+    private InetAddress inetAddress;
+    private String hostName;
+    private String hostAddress;
 
-	@RequestMapping(value = "/")
-	public String get() {
-		log.info("Ribbon verificando...");
-		return null;
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackSideApplication.class, args);
+    }
 
-	@RequestMapping(value = "/host")
-	public String host() {
-		try {
-			InetAddress inetAddress = InetAddress.getLocalHost();
-			StringBuilder result = new StringBuilder();
-			result.append("<b>IP Address:</b>").append(inetAddress.getHostAddress()).append("<br>");
-			result.append("<b>Host Name:</b>").append(inetAddress.getHostName()).append("<br>");
-			result.append("<b>Port:</b> ").append(environment.getProperty("server.port")).append("<br>");
+    @PostConstruct
+    private void init() {
+        try {
+            this.inetAddress = InetAddress.getLocalHost();
 
-			result.append("<b>Properties:</b>");
-			result.append("<ul>");
-			for (Object propertyKeyName : System.getProperties().keySet()) {
-				result.append("<li>");
-				result.append(propertyKeyName).append(": ").append(System.getProperty(propertyKeyName.toString()));
-				result.append("</li>");
-			}
-			result.append("</ul>");
+            this.hostName = inetAddress.getHostName();
+            this.hostAddress = inetAddress.getHostAddress();
+        } catch (UnknownHostException e) {
+            log.warn("Cannot get the name of localhost!", e);
+        }
+    }
 
-			return result.toString();
-		} catch (UnknownHostException e) {
-			return e.toString();
-		}
-	}
+    @RequestMapping(value = "/")
+    public String get() {
+        return "netflex-ribbon-client";
+    }
+
+    @RequestMapping(value = "/host")
+    public String host() {
+        StringBuilder result = new StringBuilder();
+
+        result.append("<b>IP Address:</b>").append(this.hostAddress).append("<br>");
+        result.append("<b>Host Name:</b>").append(this.hostName).append("<br>");
+        result.append("<b>Port:</b> ").append(environment.getProperty("server.port")).append("<br>");
+
+        result.append("<b>Properties:</b>");
+        result.append("<ul>");
+
+        for (Object propertyKeyName : System.getProperties().keySet()) {
+            result.append("<li>");
+            result.append(propertyKeyName).append(": ").append(System.getProperty(propertyKeyName.toString()));
+            result.append("</li>");
+        }
+
+        result.append("</ul>");
+
+        return result.toString();
+    }
 
 }
